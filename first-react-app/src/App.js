fe500960ab27789997d6f48d8bc497ea7eb16271
Dhruv <samantdhruv@gmail.com>
2021-09-16 15:00:43 +0530

    updated for mark8

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,40 +1,41 @@
-import React, {useState} from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./styles.css";
 
 const emojiDictionary = {
   "😉": "Winking Face",
   "😗": "Kissing Face",
   "😁": "Smiling Eyes",
-  "😶":"No Face",
+  "😶": "No Face",
   "🤥": "Lying Face",
   "🥺": "Pleading Face",
-  "😎": "Smiling Face with Sunglasses"
+  "😎": "Smiling Face with Sunglasses",
+  "🙃": "Upside Down Face",
+  "🤣": "Rolling on the Floor Laughing",
+  "😋": "Face Savoring Food"
 };
 
-var emojisWeKnow = (Object.keys(emojiDictionary));
+var emojisWeKnow = Object.keys(emojiDictionary);
 
 export default function App() {
-
-  const [ meaning, setMeaning ] = useState("")
+  const [meaning, setMeaning] = useState("");
 
   function emojiInputHandler(event) {
     var userInput = event.target.value;
 
     var meaning = emojiDictionary[userInput];
 
-    if( meaning === undefined ) {
+    if (meaning === undefined) {
       meaning = "We Dont Have This In Our DataBase";
     }
 
-    setMeaning(meaning)
+    setMeaning(meaning);
   }
-
 
   function emojiClickHandler(emoji) {
     var meaning = emojiDictionary[emoji];
     setMeaning(meaning);
   }
-  
+
   return (
     <div className="App">
       <h1>Emoji Interpreter</h1>
@@ -42,11 +43,20 @@ export default function App() {
       <h2>{meaning}</h2>
       <h3>Emojis We Know</h3>
       {emojisWeKnow.map(function (emoji) {
-        return <span
-        onClick={() => emojiClickHandler(emoji)}
-        style={{ fontSize: "large", padding: "0.5rem 0.5rem", cursor: "pointer"}} key={emoji}>{emoji}</span>;
-      } ) }
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{
+              fontSize: "large",
+              padding: "0.5rem 0.5rem",
+              cursor: "pointer"
+            }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
-  )
+  );
 }
-
